75db7b4955d22f0161c1ad71aabf3b91997062ea
Szymon Polanowski <szympol@hotmail.com>
2019-03-15 15:41:25 +0100

    Seperate Route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Route, hashHistory } from "react-router";
+import { Router, hashHistory } from "react-router";
 import { Provider } from "react-redux";
 import store from "./store";
 import DevTools from "./DevTools";
 import * as serviceWorker from "./serviceWorker";
 import { getCountries } from "./actions/actions-countries";
-import Navigation from "./presentational/navigation.component";
+import routes from './routes';
 
 render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={Navigation} />
-    </Router>
+    <Router history={hashHistory} routes={routes}/>
     <div>
       <DevTools />
     </div>
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route } from 'react-router';
+import Navigation from './presentational/navigation.component';
+
+export default (
+    <Route path='/' component={Navigation}>
+    </Route>
+);

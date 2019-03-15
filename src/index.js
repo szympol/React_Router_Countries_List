b76dbf722ebd91aed5b2329cc6e4b2e57f68e28d
Szymon Polanowski <szympol@hotmail.com>
2019-03-15 14:44:01 +0100

    Connect actions and reducers to store, add DevTools

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import DevTools from './DevTools';
 import * as serviceWorker from './serviceWorker';
+import {getCountries} from './actions/actions-countries';
 
-ReactDOM.render(<h1>Inicjalizacja projektu</h1>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <div>
+      <h1>Inicjalizacja projektu</h1>
+      <DevTools/>
+    </div>
+</Provider>, document.getElementById('root'));
+
+
+store.dispatch(getCountries());
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

522db5e72d8ca3a005e0f72eef7a798f4c03ab50
Szymon Polanowski <szympol@hotmail.com>
2019-03-15 15:33:17 +0100

    Add Navigation and Route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import DevTools from './DevTools';
-import * as serviceWorker from './serviceWorker';
-import {getCountries} from './actions/actions-countries';
+import React from "react";
+import { render } from "react-dom";
+import { Router, Route, hashHistory } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store";
+import DevTools from "./DevTools";
+import * as serviceWorker from "./serviceWorker";
+import { getCountries } from "./actions/actions-countries";
+import Navigation from "./presentational/navigation.component";
 
-ReactDOM.render(<Provider store={store}>
+render(
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={Navigation} />
+    </Router>
     <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools/>
+      <DevTools />
     </div>
-</Provider>, document.getElementById('root'));
-
+  </Provider>,
+  document.getElementById("root")
+);
 
 store.dispatch(getCountries());
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
